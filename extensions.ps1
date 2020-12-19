0c7c90b49f63ccbea9553dde760383b6b6011dbf
Alvin Luchmun <alvin@xeontek.com>
2020-12-19 15:32:19 +0000

    Added extensions

---
+++ extensions.ps1
@@ -0,0 +1,23 @@
+Write-Host " "
+Write-Host "Installing vscode extensions"
+
+code --install-extension "ms-dotnettools.csharp"
+code --install-extension "ms-mssql.mssql"
+code --install-extension "ms-vscode-remote.vscode-remote-extensionpack"
+code --install-extension "ms-vscode.powershell"
+code --install-extension "ms-azuretools.vscode-docker"
+
+code --install-extension "daylerees.rainglow"
+code --install-extension "emmanuelbeziat.vscode-great-icons"
+
+code --install-extension "jchannon.csharpextensions"
+code --install-extension "leopotam.csharpfixformat"
+code --install-extension "patcx.vscode-nuget-gallery"
+code --install-extension "derivitec-ltd.vscode-dotnet-adapter"
+code --install-extension "heaths.vscode-guid"
+
+code --install-extension "davidanson.vscode-markdownlint"
+code --install-extension "shd101wyy.markdown-preview-enhanced"
+code --install-extension "redhat.vscode-yaml"
+Write-Host " "
+Write-Host "Finished!"
